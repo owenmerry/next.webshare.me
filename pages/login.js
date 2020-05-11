5e165f76d2f6861c0504e04a4db997baceab8afc
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2020-05-11 20:21:43 +0100

    updated login page

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,19 @@
 import Menu from '../components/Menu';
-import { Button } from 'owenmerry-designsystem';
+import { Button, ProfileTitle } from 'owenmerry-designsystem';
+import { postData } from '../helpers/general';
 
 const Login = props => {
 
-    const loginUser = () => {
+    const loginUser = async () => {
         postData('http://www.webshare.me/api/user/login', { email: 'me@owenmerry.com', password: 'password' })
         .then(data => {
             console.log('post data',data); // JSON data parsed by `response.json()` call
         });
+    }
+
+    const logoutUser = async () => {
+        const res = await fetch('http://www.webshare.me/api/user/logout',{credentials: 'include'});
+        const data = await res.json();
     }
 
 return (
@@ -18,6 +24,9 @@ return (
       />
       <div>
           <Button onClick={loginUser}>Login</Button>
+          <br />
+          <br />
+          <Button onClick={logoutUser}>Logout</Button>
       </div>
     </div>
   )
