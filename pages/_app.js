03203114008665e3f3c8f90f1555af112734d616
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2020-10-21 22:18:42 +0100

    removed console logs

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import App from 'next/app';
 import * as Sentry from '@sentry/node';
 
-console.log('sentry check..',process.env.NEXT_PUBLIC_SENTRY_DSN);
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  console.log('sentry '+ process.env.NODE_ENV +'..');
   Sentry.init({
     enabled: process.env.NODE_ENV === 'production',
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN
